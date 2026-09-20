a4361824ce1ecf5c5f2d3df817e8000b7b36153c
Karl Christian Karlsson Korbacz <kodman1991@hotmail.com>
2026-09-20 09:44:58 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main>
+      <div className="w-screen h-screen flex justify-center items-center">
+        <h1 className="font-black text-9xl">HELLO WORLD!</h1>
+      </div>
+    </main>
+  )
+}
+
+export default App
